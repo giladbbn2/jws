f337f16d76ec125e75e7e6ef4c8ff479eef0e5dc
G B <giladbbn2@gmail.com>
2018-08-08 19:25:46 +0300

    javaws

--- src/main/java/javaws/BaseController.java
+++ src/main/java/javaws/BaseController.java
@@ -3,6 +3,7 @@ package javaws;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class BaseController {
