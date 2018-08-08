0c419ccae6f9ea040deadfee5cb0f1bfa2848e4e
G B <giladbbn2@gmail.com>
2018-08-08 18:26:02 +0300

    javaws

---
+++ scripts/service/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /var/www/html
+
+export JAVA_HOME=/usr/java/default; sudo /usr/maven/apache-maven-3.5.4/bin/mvn package
--- scripts/service/start.sh
+++ scripts/service/start.sh
@@ -2,8 +2,4 @@
 
 /var/www/html/scripts/stop_server.sh
 
-cd /var/www/html
-
-export JAVA_HOME=/usr/java/default; sudo /usr/maven/apache-maven-3.5.4/bin/mvn package
-
 sudo nohup java -jar /var/www/html/target/javaws-0.1.0.jar > /dev/null 2>&1 & echo $! | sudo tee /run/javaws.pid
