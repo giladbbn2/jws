cb42b3b071a22509baa1efc7bcad87c6c915566a
G B <giladbbn2@gmail.com>
2018-08-08 18:28:40 +0300

    javaws

--- scripts/service/start.sh
+++ scripts/service/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/var/www/html/scripts/stop_server.sh
+/var/www/html/scripts/service/stop_server.sh
 
 sudo nohup java -jar /var/www/html/target/javaws-0.1.0.jar > /dev/null 2>&1 & echo $! | sudo tee /run/javaws.pid
