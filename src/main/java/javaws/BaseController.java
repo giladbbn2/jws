48fed0d0549f343b5e46aac1eecdd9f5fe9d17df
G B <giladbbn2@gmail.com>
2018-08-08 19:29:59 +0300

    javaws

--- src/main/java/javaws/BaseController.java
+++ src/main/java/javaws/BaseController.java
@@ -3,13 +3,15 @@ package javaws;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 @RestController
 public class BaseController {
 
     @RequestMapping("/ws/{wspath}")
-    public String index(@PathVariable String wspath, HttpServletResponse response) {
+    public String index(@PathVariable String wspath, HttpServletRequest request, HttpServletResponse response) {
 
         return wspath;
 
